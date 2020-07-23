7e207a5acc7a70490c133cec31baa31db884118d
linchuhao <370356141@qq.com>
2020-07-23 23:19:04 +0800

    [Guess Game][Lester Lin] feature: finish the output  of validation.

--- src/main/java/example/GuessGame.java
+++ src/main/java/example/GuessGame.java
@@ -5,6 +5,27 @@ import java.util.Set;
 
 public class GuessGame {
     public String validation(int[] answer, int[] inputGuess) {
-        return null;
+        int sumOfA = 0;
+        int sumOfB = 0;
+        for (int i = 0; i < answer.length; i++){
+            if (inputGuess[i] == answer[i]){
+                sumOfA += 1;
+            }
+            if (inputGuess[i] != answer[i]){
+                if(searchNum(inputGuess[i],answer)){
+                    sumOfB += 1;
+                }
+            }
+        }
+        return sumOfA + "A" + sumOfB + "B";
+    }
+
+    private boolean searchNum(int x, int[] array) {
+        for (int index :array){
+            if (x == index){
+                return true;
+            }
+        }
+        return false;
     }
 }
